be1e8f3ce3851c729d5acab7cebb9c3f197b9628
Afeera Amir <130848741+Afeera-Amir@users.noreply.github.com>
2023-04-19 14:21:17 +0500

    update initial files

---
+++ Password Generator/index.js
@@ -0,0 +1,22 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+let passwordOne = document.getElementById("password-one")
+let passwordTwo = document.getElementById("password-two")
+
+function passwordGenerator(){
+    removePrevious()
+    for(let i = 0; i < 15; i++ ){
+        let index1 = Math.floor(Math.random() * characters.length)
+        let index2 = Math.floor(Math.random() * characters.length)
+        passwordOne.textContent += characters[index1]
+        passwordTwo.textContent += characters[index2]
+    }
+}
+function removePrevious(){
+    passwordOne.textContent = ""
+    passwordTwo.textContent = ""
+}
+
+
+
+
